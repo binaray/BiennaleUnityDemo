6eb4daa1d82b9b6f641020656b3463c3abd79f9b
kujira <raychengcx@live.com>
2020-04-16 15:49:44 +0800

    background transition functions and ui state machine

---
+++ Assets/Scripts/ArTrackableEventHandler.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+///
+/// Changes made to this file could be overwritten when upgrading the Vuforia version.
+/// When implementing custom event handler behavior, consider inheriting from this class instead.
+/// </summary>
+public class ArTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    #region PROTECTED_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName +
+                  " " + mTrackableBehaviour.CurrentStatus +
+                  " -- " + mTrackableBehaviour.CurrentStatusInfo);
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+        Debug.Log("Current state: "+UiCanvasManager.Instance.currentState);
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        if (mTrackableBehaviour)
+        {
+            Debug.LogWarning("State changed to: " + UiCanvasManager.Instance.lastRecentState);
+            UiCanvasManager.Instance.ChangeState(UiCanvasManager.Instance.lastRecentState);
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Enable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = true;
+
+            // Enable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = true;
+
+            // Enable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = true;
+        }
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        if (mTrackableBehaviour)
+        {
+            UiCanvasManager.Instance.ChangeState(UiCanvasManager.GameState.ScanQrScreen);
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Disable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = false;
+
+            // Disable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = false;
+
+            // Disable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = false;
+        }
+    }
+
+    #endregion // PROTECTED_METHODS
+}
+
--- Assets/Scripts/UiCanvasManager.cs
+++ Assets/Scripts/UiCanvasManager.cs
@@ -5,14 +5,83 @@ using UnityEngine.UI;
 
 public class UiCanvasManager : MonoBehaviour
 {
+    public enum GameState
+    {
+        ScanQrScreen,
+        StartScreen,
+        ParcelationVisScreen
+    }
+    [HideInInspector]
+    public GameState currentState;
+    [HideInInspector]
+    public GameState lastRecentState;  //used as a saved state to return when qr screen pops up
+                                        //return state is handled by ArTrackableEventHandler
+
     [SerializeField]
-    private GameObject selectLocation;
+    private GameObject background;
+    private int bgState = 0;
     [SerializeField]
-    private GameObject buttonPrefab;
+    private float bgTransitionTime = .5f;
+    private IEnumerator BgStateTransition(GameState newBgState, System.Action<bool> callback)
+    {
+        RawImage r = background.GetComponent<RawImage>();
+        float currentBlur = r.material.GetFloat("_Size");
+        Vector4 currentAdditive_Color = r.material.GetColor("_AdditiveColor");
+        Vector4 currentMultiply_Color = r.material.GetColor("_MultiplyColor");
+        float targetBlur;
+        Vector4 targetAdditive_Color;
+        Vector4 targetMultiplyColor;
+
+        switch (newBgState)
+        {
+            case GameState.ParcelationVisScreen:
+                targetBlur = 0;
+                targetAdditive_Color = new Color(0, 0, 0);
+                targetMultiplyColor = new Color(1, 1, 1);
+                break;
+            case GameState.ScanQrScreen:
+                targetBlur = 1;
+                targetAdditive_Color = new Color(0.5f, 0.5f, 0.5f);
+                targetMultiplyColor = new Color(0.5f, 0.5f, 0.5f);
+                break;
+            default:
+                targetBlur = 3;
+                targetAdditive_Color = new Color(0.5f, 0.5f, 0.5f);
+                targetMultiplyColor = new Color(0.5f, 0.5f, 0.5f);
+                break;
+            case GameState.StartScreen:
+                targetBlur = 30;
+                targetAdditive_Color = new Color(0.6f, 0.6f, 0.6f);
+                targetMultiplyColor = new Color(0.5f, 0.5f, 0.5f);
+                break;
+        }
+        float deltaBlur = (targetBlur - currentBlur) / bgTransitionTime;
+        Vector4 deltaAdditive_Color = (targetAdditive_Color - currentAdditive_Color) / bgTransitionTime;
+        Vector4 deltaMultiply_Color = (targetMultiplyColor - currentMultiply_Color) / bgTransitionTime;
+
+        float totalTime = 0;
+        while (totalTime <= bgTransitionTime)
+        {
+            //countdownImage.fillAmount = totalTime / bgTransitionTime;
+            r.material.SetFloat("_Size", currentBlur += deltaBlur * Time.deltaTime);
+            r.material.SetColor("_AdditiveColor", currentAdditive_Color += deltaAdditive_Color * Time.deltaTime);
+            r.material.SetColor("_MultiplyColor", currentMultiply_Color += deltaMultiply_Color * Time.deltaTime);
+            totalTime += Time.deltaTime;
+            yield return null;
+        }
+        r.material.SetFloat("_Size", targetBlur);
+        r.material.SetColor("_AdditiveColor", targetAdditive_Color);
+        r.material.SetColor("_MultiplyColor", targetMultiplyColor);
+        Debug.Log("UI background changed");
+        callback(true);
+    }
+
     [SerializeField]
-    private string[] unitTypes;
+    private GameObject startScreen;
     [SerializeField]
-    private DialogManager dialogManager;
+    private GameObject scanQrScreen;
+    [SerializeField]
+    private GameObject parcelationVisScreen;
 
     public static UiCanvasManager Instance { get; private set; }
     private void Awake()
@@ -27,48 +96,50 @@ public class UiCanvasManager : MonoBehaviour
         }
     }
 
-    //TODO: animation transitions
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator ScanQrScreenState()
     {
-        /*//setup selectUnitType child objects
-        GameObject unitTypeButtons = GetChildWithName(selectUnitType, "UnitTypeButtons");
-        for (int i = 0; i < unitTypes.Length; i++)
+        scanQrScreen.SetActive(true);
+        while (currentState == GameState.ScanQrScreen)
         {
-            int unitType = i + 1;
-            GameObject unitTypeButton = Instantiate(buttonPrefab, unitTypeButtons.transform, false);
-            unitTypeButton.GetComponent<RectTransform>().localPosition = new Vector3(0, i * -37, 0);
-            unitTypeButton.GetComponent<Button>().onClick.AddListener(() => { BuildingBlockManager.Instance.UnitTypeSelection = unitType; });
-            Text text = GetChildWithName(unitTypeButton, "Text").GetComponent<Text>();
-            text.text = unitTypes[i];
+            yield return null;
         }
-        Button button = GetChildWithName(selectUnitType, "ButtonCancel").GetComponent<Button>();
-        button.onClick.AddListener(CloseAll);
-
-        //setup selectLocation child objects
-        button = GetChildWithName(selectLocation, "ButtonBack").GetComponent<Button>();
-        button.onClick.AddListener(SelectUnitTypeState);
-        button = GetChildWithName(selectLocation, "ButtonConfirm").GetComponent<Button>();
-        button.onClick.AddListener(SubmissionFormState);
-
-        //setup submissionForm child objects
-        button = GetChildWithName(submissionForm, "ButtonCancel").GetComponent<Button>();
-        button.onClick.AddListener(SelectUnitTypeState);
-        button = GetChildWithName(submissionForm, "ButtonSubmit").GetComponent<Button>();
-        button.onClick.AddListener(BuildingBlockManager.Instance.PublishConfirmedBlocks);
-        button.onClick.AddListener(CloseAll);
-
-        openUnitSelectionButton.SetActive(true);
-        selectUnitType.SetActive(false);
-        selectLocation.SetActive(false);
-        submissionForm.SetActive(false);
-        //*/
+        scanQrScreen.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator StartScreenState()
     {
-        
+        startScreen.SetActive(true);
+        while (currentState == GameState.StartScreen)
+        {
+            yield return null;
+        }
+        startScreen.SetActive(false);
+    }
+
+    IEnumerator ParcelationVisScreenState()
+    {
+        parcelationVisScreen.SetActive(true);
+        while (currentState == GameState.ParcelationVisScreen)
+        {
+            yield return null;
+        }
+        parcelationVisScreen.SetActive(false);
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        if (newState != GameState.ScanQrScreen)
+            lastRecentState = newState;
+        Debug.LogWarning("Last recent state: " + lastRecentState);
+        currentState = newState;
+        StartCoroutine(BgStateTransition(newState, isCompleted => {
+            StartCoroutine(newState.ToString() + "State");
+        }));
+    }
+
+    public void ParcelationVisScreen()
+    {
+        ChangeState(GameState.ParcelationVisScreen);
     }
     
     GameObject GetChildWithName(GameObject obj, string name)
